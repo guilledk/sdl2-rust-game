9014d5aad54203fae22e580472613cc092a5903e
Guillermo Rodriguez <guillermo.rodriguez.utu@gmail.com>
2016-07-20 15:07:01 -0300

    60 fps game loop

--- src/gamestate.rs
+++ src/gamestate.rs
@@ -1,5 +1,45 @@
-pub struct GameState {
+extern crate sdl2;
+
+use sdl2::render::Texture;
+use sdl2::rect::Rect;
+use sdl2::render::Renderer;
+
+pub struct GameState<'a> {
     pub keys: [bool; 5],
+    pub logo: Sprite<'a>,
+}
+
+pub struct RectF {
+    pub x: f32,
+    pub y: f32,
+    pub w: f32,
+    pub h: f32,
+}
+
+impl RectF {
+    pub fn to_sdl_rect(&self) -> Rect {
+        Rect::new(
+            self.x.round() as i32,
+            self.y.round() as i32,
+            self.w.round() as u32,
+            self.h.round() as u32
+        )
+    }
+}
+
+pub struct Sprite<'a> {
+
+    pub img: &'a Texture,
+    pub rect: RectF,
+
+}
+
+impl<'a> Sprite<'a> {
+
+    pub fn draw(&self, renderer: &mut Renderer) {
+        renderer.copy(&self.img, None, Some(self.rect.to_sdl_rect()));
+    }
+
 }
 
 /*
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
+extern crate time;
 extern crate sdl2;
 extern crate sdl2_image;
 
+use time::{Duration, PreciseTime};
+
 mod gamestate;
 use gamestate::GameState;
+use gamestate::Sprite;
+use gamestate::RectF;
 
 use std::path::Path;
+use std::thread;
+
 use sdl2::event::Event;
-use sdl2::rect::Rect;
+use sdl2::render::Renderer;
 use sdl2::keyboard::Keycode;
 
 use sdl2_image::{LoadTexture, INIT_PNG, INIT_JPG};
 
 fn main() {
+
     println!("Hello, world!");
 
     let ctx = sdl2::init().unwrap();
@@ -30,30 +38,55 @@ fn main() {
     .build()
     .unwrap();
 
-    let dst = Rect::new(400 - 256, 300 - 256, 512, 512);
+    let mut events = ctx.event_pump().unwrap();
     let texture = renderer.load_texture(&Path::new("res/rust-logo.png")).unwrap();
 
-    renderer.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
-    renderer.clear();
-    renderer.copy(&texture, None, Some(dst));
+    let mut gs = GameState {
+        logo: Sprite { img: &texture, rect: RectF { x: -512.0, y: 300.0 - 256.0, w: 512.0, h: 512.0 } },
+        keys: [false, false, false, false, false]
+    };
 
-    renderer.present();
-
-    let mut events = ctx.event_pump().unwrap();
-
-    let mut gs = GameState { keys: [false, false, false, false, false] };
 
     'event : loop {
+
+        let start = PreciseTime::now();
+
         if gs.keys[0] { break 'event }
         for event in  events.poll_iter() {
             match event {
                 Event::Quit{..}    => break 'event,
-                Event::KeyDown{keycode: Some(keycode), ..} => { keydown(keycode, &mut gs); println!("keys: {:?}", gs.keys) },
-                Event::KeyUp{keycode: Some(keycode), ..}   => { keyup(keycode, &mut gs); println!("keys: {:?}", gs.keys) },
+                Event::KeyDown{keycode: Some(keycode), ..} => keydown(keycode, &mut gs),
+                Event::KeyUp{keycode: Some(keycode), ..}   => keyup(keycode, &mut gs),
                 _                  => continue
             }
         }
+        update(&mut gs);
+        draw(&mut renderer, &gs);
+
+        let delta: Duration = start.to(PreciseTime::now());
+
+        thread::sleep_ms((Duration::microseconds(16666) - delta).num_milliseconds() as u32);
+
     }
+
+}
+
+fn update(gs: &mut GameState) {
+
+    gs.logo.rect.x += 1000.0 / 60.0;
+    if gs.logo.rect.x > 800.0 {
+        gs.logo.rect.x = -512.0;
+    }
+
+}
+
+fn draw(renderer: &mut Renderer, gs: &GameState) {
+
+    renderer.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
+    renderer.clear();
+    gs.logo.draw(renderer);
+
+    renderer.present();
 
 }
 
